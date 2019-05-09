6f9918d5f7ef791b9e6cf4b9e1f59edec2482bf1
Daniel <nieltansah@gmail.com>
2019-05-09 23:27:09 +0700

    testsuite: evp: Add testsuite that uses Fedora image
    This exception is needed for testing md2 hash algo.

---
+++ testsuite/evp/build-fedora.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+[ $# -ne 1 ] && exit 1
+
+id=${1,,}
+
+docker build \
+  --build-arg http_proxy=http://nieltansah:39189509@cache.itb.ac.id:8080 \
+  --build-arg https_proxy=http://nieltansah:39189509@cache.itb.ac.id:8080 \
+  --build-arg evp="$id" \
+  -t "dieharder-$id" \
+  -f Dockerfile-fedora .
+docker run -d --name "dieharder-$id-1" "dieharder-$id"
